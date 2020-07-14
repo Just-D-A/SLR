20cd9cf8d46f4d3e27adffe3cf91a230ac8ec29f
Just-D-A <prospeedcuber@gmail.com>
2020-07-15 01:51:36 +0300

    change runner

--- src/main/org/volgatech/Main.java
+++ src/main/org/volgatech/Main.java
@@ -26,19 +26,108 @@ public class Main {
         Converter converter = new Converter(grammar);
         ArrayList<ArrayList<String>> grammarMap = converter.convertGrammar();
         //System.out.println("*** Table complited ***");
+        ArrayList<ArrayList<String>> testTables = new ArrayList<>();
+////Create line 0
+        ArrayList<String> testString0 = new ArrayList<>();
+        testString0.add(";");
+        testString0.add("<S>");
+        testString0.add("<idlist>");
+        testString0.add("<id>");
+        testString0.add("real");
+        testString0.add(",");
+        testString0.add("A|B|C");
+        testString0.add("@");
+        testTables.add(testString0);
+//Create line 1
+        ArrayList<String> testString1 = new ArrayList<>();
+        testString1.add("<S>");
+        testString1.add("ok");
+        testString1.add(";");
+        testString1.add(";");
+        testString1.add("real11");
+        testString1.add(";");
+        testString1.add(";");
+        testString1.add(";");
+        testTables.add(testString1);
+//Create line 2
+        ArrayList<String> testString2 = new ArrayList<>();
+        testString2.add("real11");
+        testString2.add(";");
+        testString2.add("<idlist>21|<idlist>12");
+        testString2.add("<id>31");
+        testString2.add(";");
+        testString2.add(";");
+        testString2.add("A|B|C41");
+        testString2.add(";");
+        testTables.add(testString2);
+//Create line 3
+        ArrayList<String> testString3 = new ArrayList<>();
+        testString3.add("<idlist>21|<idlist>12");
+        testString3.add(";");
+        testString3.add(";");
+        testString3.add(";");
+        testString3.add(";");
+        testString3.add(",22");
+        testString3.add(";");
+        testString3.add("R1");
+        testTables.add(testString3);
+//Create line 4
+        ArrayList<String> testString4 = new ArrayList<>();
+        testString4.add("<id>31");
+        testString4.add(";");
+        testString4.add(";");
+        testString4.add(";");
+        testString4.add(";");
+        testString4.add("R3");
+        testString4.add(";");
+        testString4.add("R3");
+        testTables.add(testString4);
+//Create line 5
+        ArrayList<String> testString5 = new ArrayList<>();
+        testString5.add("A|B|C41");
+        testString5.add(";");
+        testString5.add(";");
+        testString5.add(";");
+        testString5.add(";");
+        testString5.add("R4");
+        testString5.add(";");
+        testString5.add("R4");
+        testTables.add(testString5);
+//Create line 6
+        ArrayList<String> testString6 = new ArrayList<>();
+        testString6.add(",22");
+        testString6.add(";");
+        testString6.add(";");
+        testString6.add("<id>23");
+        testString6.add(";");
+        testString6.add(";");
+        testString6.add("A|B|C41");
+        testString6.add(";");
+        testTables.add(testString6);
+//Create line 7
+        ArrayList<String> testString7 = new ArrayList<>();
+        testString7.add("<id>23");
+        testString7.add(";");
+        testString7.add(";");
+        testString7.add(";");
+        testString7.add(";");
+        testString7.add("R2");
+        testString7.add(";");
+        testString7.add("R2");
+        testTables.add(testString7);
 
-        /********************************************************
-         *                                                      *
-         *      ЯРОСЛАВ, ПОЖАЛУЙСТА, ПИШИ КОД В КОНВЕРКТОРЕ     *
-         *                                                      *
-         *     ПУТЬ SRC.MAIN.ORG.VOLGATECH.TABLE.CONVECTOR      *
-         *                                                      *
-         *     ПРОСТО Я МЕЙН БЫВАЮТ МЕНЯЮ И БОЮСЬ МЕРДЖИТЬ      *
-         ********************************************************
 
+        Runner runner = new Runner(testTables, tokenList, grammar);
+        Token errorToken = runner.run();
+        if (errorToken == null) {
+            System.out.println("Complited!");
+        } else {
+            System.out.println("ERROR");
+            errorToken.writeToken();
+        }
 
-       /*     System.out.println("*** Table complited ***");
-        Runner runner = new Runner(tokenList, methods);
+     /*  System.out.println("*** Table complited ***");
+        Runner runner = new Runner(testTables, tokenList, grammar);
         Token errorToken = runner.run();
         if (errorToken == null) {
             System.out.println("Complited!");
@@ -46,7 +135,5 @@ public class Main {
             System.out.println("ERROR");
             errorToken.writeToken();
         }*/
-
-
     }
 }
--- src/main/org/volgatech/runner/Runner.java
+++ src/main/org/volgatech/runner/Runner.java
@@ -9,18 +9,22 @@ import java.util.Stack;
 
 public class Runner {
     private ArrayList<ArrayList<String>> table;
+    ArrayList<ArrayList<String>> grammar;
     ArrayList<Token> tokens;
     Stack<String> stack;
     int currStringsIndex;
+    int currTokenIndex;
+    Token currToken;
     Token nextToken;
 
-    public Runner(ArrayList<ArrayList<String>> table, ArrayList<Token> tokens) {
+    public Runner(ArrayList<ArrayList<String>> table, ArrayList<Token> tokens, ArrayList<ArrayList<String>> grammar) {
         this.table = table;
         this.tokens = tokens;
+        this.grammar = grammar;
         Stack<String> stack = new Stack<>();
         currStringsIndex = 1;
+        currTokenIndex = 0;
         fixTokenList();
-
     }
 
     private void fixTokenList() {
@@ -34,13 +38,121 @@ public class Runner {
     }
 
     public Token run() {
+        tokens.add(new Token(0, "@", 0, 0));
+        for(Token token: tokens) {
+            System.out.println(token.getValue() + " ");
+        }
+        System.out.println("__TOKENS END__");
+        shift();
+        String result = goToNext();
+        //writeOutStack();
+        return result.equals("ok") ? null : currToken;
+      //  return null;
+    }
+
+    private String goToNext() {
+        System.out.println("WITH TOKEN " + currToken.getValue());
+        String valOfCell = "";
+        String upValOfCell = "";
 
 
+        ArrayList<String> localCurrentStr = table.get(currStringsIndex);
+        if(isTerminal(localCurrentStr.get(0))) {
+            shift();
+            System.out.println("CHANGE TOKEN " + currToken.getValue());
+        }
+        //ArrayList<String> currString = table.get(currStringsIndex);
+        int upIndex = getColomIndexByToken(currToken);
+        if(upIndex == -1) {
+            System.out.println("ERROR");
+            return "ERROR_1";
+        }
+
+        while(!valOfCell.equals(";")) {
+        //    System.out.println(upIndex);
+            valOfCell = localCurrentStr.get(upIndex);
+            if(valOfCell.equals("ok")) {
+                return "ok";
+            }
+            System.out.println(valOfCell);
+            char[] charArr = valOfCell.toCharArray();
+            if((charArr[0] == 'R') | ((valOfCell.equals(";")))) {
+                break;
+            }
+            currStringsIndex = getStringIndexByCellEl(valOfCell);
+            if (currStringsIndex == -1) {
+                System.out.println("CAN NOT FIND LEFT ELEMENT OF " + valOfCell + " CURRENT STRING INDEX " + currStringsIndex);
+                return "ERROR_2";
+            }
+
+            upValOfCell = goToNext();
+            if(upValOfCell.contains("ERROR")) {
+                return upValOfCell;
+            }
+            System.out.println(upValOfCell + " UP");
+            char[] charArrs = upValOfCell.toCharArray();
+            if(charArrs[0] == 'R') {
+                break;
+            }
+            upIndex = getUpIndexByVal(upValOfCell);
+        }
+        if((valOfCell.equals(";")) & upValOfCell.equals("")) {
+            return "ERROR_3";
+        }
+        System.out.println("IM HERE " + upValOfCell);
+      //  stack.push(token.getValue());
+        char[] charArr = valOfCell.toCharArray();
+        if(charArr[0] == 'R') {
+            String substring = valOfCell.substring(1);
+           /* int grammarSrtSize = getGrammarStringSize(Integer.parseInt(substring));
+            //     writeOutStack();
+            for(int i = 0; i < grammarSrtSize -1; i++) {
+                System.out.println("GET FROM STACK " + stack.pop());
+            }
+            //       writeOutStack();*/
+            valOfCell = getGrammarStringFirstVal(Integer.parseInt(substring));
+            //stack.push(valOfCell);
+            return valOfCell;
+        }
+        System.out.println("RETURN " + upValOfCell);
+        return upValOfCell;
+    }
+
+    private String getGrammarStringFirstVal(int i) {
+        return grammar.get(i-1).get(0);
+    }
+
+    private int getGrammarStringSize(int i) {
+        return grammar.get(i-1).size();
+    }
+
+    private Token getNextTokern(int tokenIndex) {
+        if(tokenIndex < tokens.size()) {
+            tokenIndex++;
+            return tokens.get(tokenIndex);
+        }
         return null;
     }
 
-    private boolean goToNext(Token token, int index) {
-        return true;
+    private void shift() {
+        if(currTokenIndex < tokens.size()) {
+            currToken = tokens.get(currTokenIndex);
+            currTokenIndex++;
+        }
+    }
+
+    private int getUpIndexByVal(String val) {
+        ArrayList<String> upString = table.get(0);
+        int x = -1;
+        for(int i = 0; i< upString.size();i++) {
+           // System.out.print(upString.get(i) + " ");
+            if(val.equals(upString.get(i))) {
+                x = i;
+                break;
+            }
+        }
+    //    System.out.println();
+        return x;
     }
 
     private int getStringIndexByCellEl(String valOfCell) {
@@ -48,6 +160,7 @@ public class Runner {
         for (int i = 0; i < table.size(); i++) {
             String leftPart = table.get(i).get(0);
             if (leftPart.equals(valOfCell)) {
+                System.out.println();
                 x = i;
                 break;
             }
@@ -58,7 +171,7 @@ public class Runner {
 
     private int getColomIndexByToken(Token token) {
         ArrayList<String> firstStrings = table.get(0);
-
+     //   System.out.println("Token val " + token.getValue());
         int x = -1;
 
         for (int i = 0; i < firstStrings.size(); i++) {
@@ -95,7 +208,7 @@ public class Runner {
                 ((tokenTypeIndex >= Globals.INTEGER_KEY) & (tokenTypeIndex <= Globals.FLOAT_KEY) & (guideSet.equals(Globals.GRAMMAR_AND_TOKEN_NUMBER_SYMBOL)));
     }
 
-    private boolean checkTerminal(String val) {
+    private boolean isTerminal(String val) {
         if (val.length() > 1) {
             char[] valCharArr = val.toCharArray();
             return (valCharArr[0] != '<');
